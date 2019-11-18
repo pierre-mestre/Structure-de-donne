62899a41619a781501f4fef6a0e5b763ca956bde
Pierre MESTRE <mestrep@pc5301-test.esiee.fr>
2019-11-18 14:03:27 +0100

    add sublim text unix

--- i3_in9_lib/vector_v1_double.h
+++ i3_in9_lib/vector_v1_double.h
@@ -1,5 +1,9 @@
 #ifndef _LINKEDLIST_H_
 ​​​‍﻿﻿‌​﻿#define _LINKEDLIST_H_
 
+typedef struct struct_vector_v1_double{
+	int nbElement = 0;
+	double* pointeurDouble = 0;
+}struct_vector_v1_double
 
 #en​​​‍﻿﻿‌​﻿dif
